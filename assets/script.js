c10f0ae12c7e3a84a3cdcaaa36e80e3d1f301714
alexeykuz-sys <alexeykuz@gmail.com>
2021-03-11 21:01:01 +0000

    finished challenge

--- assets/script.js
+++ assets/script.js
@@ -55,7 +55,64 @@ button.addEventListener("click", function () {
   }
 });
 
+// email validation
+
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+// elements fade in or slide in
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries,appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+  
+},appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+  // console.log(slider)
+})
+
+
+const slides = document.querySelectorAll(".slides");
+const dots = document.querySelectorAll(".dot");
+// setting the index for the slides
+let slideIndex = 0;
+
+const showSlides = () => {
+    slides.forEach(slide => {
+        slide.style.display = "none";
+    })
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    } 
+    dots.forEach(dot => {
+        dot.className = dot.className.replace("active", "");
+    })
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active"; 
+    setTimeout(showSlides, 3000);
+}
+showSlides()
